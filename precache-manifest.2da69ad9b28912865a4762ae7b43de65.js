cd2b98efc638f14d1133b042c1cb0cbf259d119d
avafitoussi <ava.sahal@allcloud.io>
2020-03-17 17:22:50 +0200

    Updates

---
+++ precache-manifest.2da69ad9b28912865a4762ae7b43de65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a5c9dcf27b1d35ed38efab6937802c",
+    "url": "/techseeapp/index.html"
+  },
+  {
+    "revision": "5008c8d5d3766be1c5c2",
+    "url": "/techseeapp/static/css/main.9fe6b29e.chunk.css"
+  },
+  {
+    "revision": "31d4dad78acf484324b3",
+    "url": "/techseeapp/static/js/2.fd52573e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/techseeapp/static/js/2.fd52573e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5008c8d5d3766be1c5c2",
+    "url": "/techseeapp/static/js/main.ac98744b.chunk.js"
+  },
+  {
+    "revision": "81066718228ec574f320",
+    "url": "/techseeapp/static/js/runtime-main.966a9326.js"
+  }
+]);
